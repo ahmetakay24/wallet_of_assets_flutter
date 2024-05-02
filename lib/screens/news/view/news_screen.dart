92c1d8965cdd5e728b1a3a6875043eab702c084b
ahmetakay24 <ahmetakay6600@gmail.com>
2024-05-02 20:50:32 +0300

    sheets and controllers added

--- lib/screens/news/view/news_screen.dart
+++ lib/screens/news/view/news_screen.dart
@@ -5,6 +5,8 @@ class NewsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
